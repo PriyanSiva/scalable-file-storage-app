910939e64f17db2a9be1662bf8dcad3dcd489b9f
Priyan S <sprverse@gmail.com>
2025-12-04 17:30:06 -0500

    Initial React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 50 }}>
+      <h1>Scalable File Storage App</h1>
+      <p>Deployment working!</p>
+    </div>
+  );
+}
+
+export default App;
