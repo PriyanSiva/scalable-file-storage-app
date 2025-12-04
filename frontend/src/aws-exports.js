a67d42e3c74ed8c72da5d5afb44b91e8d92365bf
Priyan S <sprverse@gmail.com>
2025-12-04 18:21:24 -0500

    Added Cognito config and Amplify setup

---
+++ frontend/src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+  Auth: {
+    region: "us-east-2",
+    userPoolId: "us-east-2_V9i10HMTb",
+    userPoolWebClientId: "7fhhf73hpstdu0fdrc313n4v9t",
+  },
+};
+
+export default awsconfig;
