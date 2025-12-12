ee41f1c8766bef80edc43eaa3fef37bbd99ef92e
Priyan S <sprverse@gmail.com>
2025-12-12 10:16:44 -0500

    Fix Cognito redirect for production

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "react-oidc-context";
 
+const isLocal = window.location.hostname === "localhost";
+
+const redirectUri = isLocal
+  ? "http://localhost:3000/"
+  : window.location.origin + "/";
+  
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_3sTzqaET8",
   client_id: "36fk9vns52pbma5n0brs54c2vc",
-  redirect_uri: "http://localhost:3000/",
-  post_logout_redirect_uri: "http://localhost:3000/", 
+  redirect_uri: redirectUri,
+  post_logout_redirect_uri: redirectUri, 
   response_type: "code",
   scope: "phone openid email",
 };
